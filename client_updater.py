5945a4f6964a84249e6382aac2a51dd91094ef0b
Alex <thealexmcbride@gmail.com>
2021-05-02 21:42:18 +0100

    *_

--- client_updater.py
+++ client_updater.py
@@ -3,8 +3,40 @@
 import requests
 import hashlib
 
-# data = requests.get("https://raw.githubusercontent.com/actorpus/TankTrouble/main/client.py")
-# print(data.text)
 
-with open("client.py", "rb") as file:
-    print(hashlib.sha256(file.read(), usedforsecurity=True).hexdigest())
+def replace(_file_name):
+    f = requests.get("https://raw.githubusercontent.com/actorpus/TankTrouble/main/" + _file_name)
+
+    if f.text != "404: Not Found":
+        with open(_file_name, "w") as nf:
+            nf.write(f.text)
+
+        print("Updated", _file_name)
+    else:
+        print("Could not update", _file_name)
+
+
+data = requests.get("https://raw.githubusercontent.com/actorpus/TankTrouble/main/VERSIONS")
+
+versions = {}
+
+for v in data.text.split('\n'):
+    versions[v.split(' ')[0]] = v.split(' ')[1:]
+
+print(versions)
+
+for file_name in versions.keys():
+    try:
+        file = open(file_name, "rb")
+
+        file_contents = file.read()
+
+        print(file_contents.split(b'\n')[0].split(b' ')[2].decode())
+
+        print(hashlib.sha256(file_contents, usedforsecurity=True).hexdigest())
+
+        file.close()
+
+    except FileNotFoundError:
+        replace(file_name)
+

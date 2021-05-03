a8e860d249ba4450749c355bf7909b4cb7fdc389
Alex <thealexmcbride@gmail.com>
2021-05-03 11:54:49 +0100

    *_

--- updater.py
+++ updater.py
@@ -1,20 +1,16 @@
-# updater.py V0.0.1
+# updater.py V0.0.2
 
 import requests
 import hashlib
 
 
 def replace(_file_name):
-    f = requests.get("https://raw.githubusercontent.com/actorpus/TankTrouble/main/" + _file_name)
+    f = requests.get("https://raw.githubusercontent.com/actorpus/TankTrouble/main/" + _file_name, stream=True)
 
-    if f.text != "404: Not Found":
-        with open(_file_name, "w") as nf:
-            nf.write(f.text)
-
-        print("Updated", _file_name)
-    else:
-        print("Could not update", _file_name)
-
+    if f.status_code == 200:
+        with open(_file_name, 'wb') as file:
+            for chunk in f:
+                file.write(chunk)
 
 data = requests.get("https://raw.githubusercontent.com/actorpus/TankTrouble/main/VERSIONS")
 
